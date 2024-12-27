af74add2fb5e0fb37e7ac84f3c1013804b19afc2
hvksrv <hr.beyondbeta@gmail.com>
2024-12-27 23:54:42 +0000

    logos added

--- app/components/auth/social-auth.tsx
+++ app/components/auth/social-auth.tsx
@@ -3,17 +3,19 @@
 import { Button } from "../ui/button";
 import Image from "next/image";
 
+import gLogo from "../../../public/google.png"
+
 export function SocialAuth() {
   return (
     <div className="grid gap-2">
       <Button variant="outline" className="w-full" onClick={() => {}}>
-        {/* <Image
-          src="https://www.google.com/favicon.ico"
+        <Image
+          src={gLogo}
           alt="Google"
           width={16}
           height={16}
           className="mr-2"
-        /> */}
+        />
         Continue with Google
       </Button>
     </div>
